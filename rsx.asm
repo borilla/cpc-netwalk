50d63c68ac45f5e9475dfd5da2166944ed32a1ad
borilla <github@borilla.co.uk>
2018-03-26 12:37:43 +0800

    Expand RSX test to include input & output params

--- rsx.asm
+++ rsx.asm
@@ -1,22 +1,6 @@
 ;; http://www.cpcwiki.eu/index.php/Programming:An_example_to_define_a_RSX
-;;
-;; This program provides the framework for the installation of a Resident System
-;; Extension (an RSX).
-;;
-;; An RSX is a command which is accessed from basic by prexifing the command
-;; with the '|' symbol.
-;;
-;; e.g. An RSX could provide a "MEMEDIT" command, which allows the user
-;; to edit the RAM contents. It would be accessed from BASIC by typing:
-;;
-;; |MEMEDIT
-;;
-;; This program shows how an RSX is set up. You are free to use this
-;; example code to create your own RSX's.
-;;
-;; Kevin Thacker 1993
 
-.kl_log_ext	equ &bcd1
+kl_log_ext	equ &bcd1
 
 ;; this can be any address in the range &0040-&a7ff.
 		org &8000
@@ -28,40 +12,35 @@
 		ld bc,jump_table	;; address of command name table and routine handlers
 		jp kl_log_ext		;; install RSXs
 
-.work_space	defs 4			;; space for kernel to use
 
 ;;-------------------------------------------------------------------------------
 ;; RSX definition
 
-.jump_table	defw name_table		;; address pointing to RSX commands 
-
-					;; list of jump commands associated with each command
-
-					;; The name (in the name_table) and jump instruction
-					;; (in the jump_table), must be in the same
-					;; order.
-					;; i.e. the first name in the name_table refers to the
-					;; first jump in the jump_table, and vice versa.
-
-		jp RSX_1_routine	;; routine for COMMAND1 RSX
-		jp RSX_2_routine	;; routine for COMMAND2 RSX
-		jp RSX_3_routine	;; routine for COMMAND3 RSX
-
-;; the table of RSX function names
-;; the names must be in capitals.
-
-.name_table	defb "COMMAND","1"+&80	;; the last letter of each RSX name must have bit 7 set to 1.
-		defb "COMMAND","2"+&80	;; This is used by the Kernel to identify the end of the name.
+name_table	defb "COMMAND","1"+&80
+		defb "COMMAND","2"+&80
 		defb "COMMAND","3"+&80
-		defb 0			;; end of name table marker
+		defb 0			;; marker for end of name table
 
-;; Code for the example RSXs
+work_space	defs 4			;; space for kernel to use
 
-.RSX_1_routine
-ret
+jump_table	defw name_table		;; pointer to names
+		jp RSX_1_routine	;; jump block
+		jp RSX_2_routine
+		jp RSX_3_routine
 
-.RSX_2_routine
-ret
+;;-------------------------------------------------------------------------------
+;; RSX routines
 
-.RSX_3_routine
-ret
+RSX_1_routine	ret
+
+;; |COMMAND2,a,b,@c%
+RSX_2_routine	cp 3		;; check we have expected number of params
+		ret nz
+		ld a,(ix+4)	;; load first parameter into A
+		add a,(ix+2)	;; add second parameter
+		ld h,(ix+1)	;; load HL with third parameter (address of integer var)
+		ld l,(ix+0)
+		ld (hl),a	;; put result into third parameter
+		ret
+
+RSX_3_routine	ret
