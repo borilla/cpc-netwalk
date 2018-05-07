129c1d24b1d0d502da45be2f33117e807a78800d
borilla <github@borilla.co.uk>
2018-05-08 00:18:38 +0800

    Move `setup_screen` and and `render_maze` into subroutines in `start.asm`

--- render-sprite.asm
+++ render-sprite.asm
@@ -1,48 +1,25 @@
-;; ----------------------------------------------------------------
-;; main
-;; ----------------------------------------------------------------
-
-setup_screen	wait_for_vsync
-		setup_minimal_interrupt_handler
-		;; set screen mode
-		ga_select_mode 1		;; mode 1
-		;; reset screen origin
-		crtc_set_screen_address &c000,0	;; page &c000, offset 0
-		;; setup screen resolution
-		crtc_write_register 1,32	;; horizontal displayed: 32 characters, 64 bytes, 256 mode 1 pixels
-		crtc_write_register 2,42	;; horizontal sync position
-		crtc_write_register 6,32	;; vertical displayed: 32 characters, 256 pixels
-		crtc_write_register 7,34	;; vertical sync position
-		;; setup pen colors
-		ga_set_pen 0,ink_bright_white	;; background
-		ga_set_pen 1,ink_white
-		ga_set_pen 2,ink_bright_cyan
-		ga_set_pen 3,ink_pastel_blue
-		ga_set_pen 16,ink_white		;; border
-		ret
-
 ;; render a sprite
 ;; entry:
 ;;	DE: screen address (of top-left of sprite)
 ;;	HL: sprite data
 ;; modifies:
 ;;	AF,BC,DE,HL
-render_sprite	ld bc,&800 + &3d + 24			;; we want C to equal &3d after 24 LDI instructions
+render_sprite	ld bc,&800 + &3d + 24		;; we want C to equal &3d after 24 LDI instructions
 		call _render_half_sprite
-		ld b,&c8				;; add de,&c83d (-2048 * 7 + 64 - 3)
+		ld b,&c8			;; add de,&c83d (-2048 * 7 + 64 - 3)
 		ex hl,de
 		add hl,bc
 		ex hl,de
 		ld b,8
 _render_half_sprite
 repeat 7
-		ldi:ldi:ldi				;; [15] copy three bytes
-		ld a,(hl):ld (de),a: inc l		;; [5] copy byte
-		dec e:dec e:dec e			;; [3] reset to left of sprite
-		ld a,d:add a,b:ld d,a			;; [3] add de,&800
+		ldi:ldi:ldi			;; [15] copy three bytes
+		ld a,(hl):ld (de),a: inc l	;; [5] copy byte
+		dec e:dec e:dec e		;; [3] reset to left of sprite
+		ld a,d:add a,b:ld d,a		;; [3] add de,&800
 rend
-		ldi:ldi:ldi				;; [15] copy three bytes
-		ld a,(hl):ld (de),a: inc l		;; [5] copy byte
+		ldi:ldi:ldi			;; [15] copy three bytes
+		ld a,(hl):ld (de),a: inc l	;; [5] copy byte
 		ret
 
 ;; get screen address of grid cell (origin + 4x + 128y)
@@ -95,5 +72,5 @@ sprite_from_index
 ;; ----------------------------------------------------------------
 
 align 64
-.sprite_data
+sprite_data
 read "maze/sprite-data.asm"
--- start.asm
+++ start.asm
@@ -1,16 +1,34 @@
 read "inc/macros.asm"
 
-		org &8000
-
-		di			;; disable interrupts (helps when measuring timings)
+main		di			;; disable interrupts
 		call setup_screen
-
 		xor a			;; ld a,0 (so maze will be 16 x 16)
 		call maze_generate
+		call render_maze
+wait_forever	halt
+		jr wait_forever
 
-		ld hl,maze_data
+;; set mode, screen size, colours etc
+;; disable firmware interrupts before calling
+setup_screen	;; set screen mode
+		ga_select_mode 1		;; mode 1
+		;; set screen origin
+		crtc_set_screen_address &c000,0	;; page &c000, offset 0
+		;; set screen size
+		crtc_write_register 1,32	;; horizontal displayed: 32 characters, 64 bytes, 256 mode 1 pixels
+		crtc_write_register 2,42	;; horizontal sync position
+		crtc_write_register 6,32	;; vertical displayed: 32 characters, 256 pixels
+		crtc_write_register 7,34	;; vertical sync position
+		;; set pen colors
+		ga_set_pen 0,ink_bright_white	;; background
+		ga_set_pen 1,ink_black		;; outlines
+		ga_set_pen 2,ink_lime		;; power flow
+		ga_set_pen 3,ink_white		;; tile edges
+		ga_set_pen 16,ink_white		;; border
+		ret
 
-loop		ld a,l
+render_maze	ld hl,maze_data
+_render_maze_1	ld a,l
 		call cell_screen_addr	;; DE = screen address for cell
 		ld a,(hl)
 		and %01111111
@@ -19,11 +37,8 @@ loop		ld a,l
 		call render_sprite
 		pop hl
 		inc l
-		jr z,wait_forever
-		jr loop
-
-wait_forever	halt
-		jr wait_forever
+		jr nz,_render_maze_1
+		ret
 
 read "maze/render-sprite.asm"
 read "maze/maze.asm"
