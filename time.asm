b3e44df8bd37e8c365d9922022abe3f3a5dd38dc
borilla <github@borilla.co.uk>
2018-09-11 14:26:25 +0800

    Use const for screen addr of time display

--- time.asm
+++ time.asm
@@ -1,3 +1,5 @@
+time_screen_addr	equ &c030
+
 time_init
 		xor a
 		ld hl,time_data_ms_lo
@@ -13,28 +15,28 @@ time_init
 		inc hl
 		ld (hl),a
 
-		ld de,&c030			;; min hi
+		ld de,time_screen_addr		;; min hi
 		ld hl,char_data_0
 		call char_render
-		ld de,&c032			;; min lo
+		ld de,time_screen_addr + 2	;; min lo
 		ld hl,char_data_0
 		call char_render
-		ld de,&c034
+		ld de,time_screen_addr + 4
 		ld hl,char_data_colon
 		call char_render
-		ld de,&c036			;; sec hi
+		ld de,time_screen_addr + 6	;; sec hi
 		ld hl,char_data_0
 		call char_render
-		ld de,&c038			;; sec lo
+		ld de,time_screen_addr + 8	;; sec lo
 		ld hl,char_data_0
 		call char_render
-		ld de,&c03a
+		ld de,time_screen_addr + 10
 		ld hl,char_data_colon
 		call char_render
-		ld de,&c03c			;; ms hi
+		ld de,time_screen_addr + 12	;; ms hi
 		ld hl,char_data_0
 		call char_render
-		ld de,&c03e			;; ms lo
+		ld de,time_screen_addr + 14	;; ms lo
 		ld hl,char_data_0
 		call char_render
 		ret
@@ -47,7 +49,7 @@ time_inc_ms_lo
 		ld a,&fe
 _tims_lo	add a,2
 		ld (time_data_ms_lo),a
-		ld de,&c03e
+		ld de,time_screen_addr + 14
 		call char_render_digit
 		ret
 
@@ -61,7 +63,7 @@ time_inc_ms_hi
 		ld a,&ff
 _tims_hi	inc a
 		ld (time_data_ms_hi),a
-		ld de,&c03c
+		ld de,time_screen_addr + 12
 		call char_render_digit
 		ret
 
@@ -75,7 +77,7 @@ time_inc_sec_lo
 		ld a,&ff
 _tis_lo		inc a
 		ld (time_data_sec_lo),a
-		ld de,&c038
+		ld de,time_screen_addr + 8
 		call char_render_digit
 		ret
 
@@ -89,7 +91,7 @@ time_inc_sec_hi
 		ld a,&ff
 _tis_hi		inc a
 		ld (time_data_sec_hi),a
-		ld de,&c036
+		ld de,time_screen_addr + 6
 		call char_render_digit
 		ret
 
@@ -103,7 +105,7 @@ time_inc_min_lo
 		ld a,&ff
 _tim_lo		inc a
 		ld (time_data_min_lo),a
-		ld de,&c032
+		ld de,time_screen_addr + 2
 		call char_render_digit
 		ret
 
@@ -117,7 +119,7 @@ time_inc_min_hi
 		ld a,&ff
 _tim_hi		inc a
 		ld (time_data_min_hi),a
-		ld de,&c030
+		ld de,time_screen_addr
 		call char_render_digit
 		ret
 
