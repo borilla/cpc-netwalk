0d86e55d02eaa4f12d87857abfade8f0436d925f
borilla <>
2025-03-28 04:19:47 +0000

    Build before launching project in emulator
    - If dev-container is running then run makefile in the container before launching emulator
    - If container isn't running then just launch

--- build/launch-in-ace.sh
+++ build/launch-in-ace.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Open built game in Ace-DL emulator
-# Assumes Ace-DL is installed in a fixed location so is unlikely to work generally
+# attempt to run makefile in the dev container [Does nothing if container isn't running]
+docker ps | grep vsc-netwalk | awk '{print $1}' | head -1 | xargs -i docker exec {} make --directory /workspaces/netwalk
+
+# cd to build directory
 cd "$(dirname "$0")"
-~/Amstrad\ CPC/Emulators/AceDL/AceDL netwalk.rasm netwalk.dsk -autoRunFile 'netwalk.bin'
+
+# open and run disk image in Ace-DL emulator [Assumes Ace-DL is installed in a fixed location so is unlikely to work generally]
+~/Amstrad\ CPC/Emulators/AceDL/AceDL netwalk.rasm netwalk.dsk -autoRunFile 'netwalk'
--- build/launch-in-winape.sh
+++ build/launch-in-winape.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-# Open built game in WinAPE emulator
-# Assumes WinAPE is set to run on through a specific configuration within bottles so is unlikely to work generally
-flatpak run --command=bottles-cli com.usebottles.bottles run -p WinApe -b 'Win10' -- /A:netwalk.bin /SYM:'D:\Asm\\netwalk\\build\\netwalk.sym' 'D:\Asm\\netwalk\\build\\netwalk.dsk'
+# attempt to run makefile in the dev container [Does nothing if container isn't running]
+docker ps | grep vsc-netwalk | awk '{print $1}' | head -1 | xargs -i docker exec {} make --directory /workspaces/netwalk
+
+# cd to build directory
+cd "$(dirname "$0")"
+
+# open and run disk image in WinAPE emulator [Assumes WinAPE is set to run on through a specific configuration within bottles so is unlikely to work generally]
+flatpak run --command=bottles-cli com.usebottles.bottles run -p WinApe -b 'Win10' -- /A:netwalk /SYM:'D:\Asm\\netwalk\\build\\netwalk.sym' 'D:\Asm\\netwalk\\build\\netwalk.dsk'
